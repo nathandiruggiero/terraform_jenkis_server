09f3cd0815ef7afb07f10deb689fc60a47dc6784
Nathan Di ruggiero <58921322+nathandiruggiero@users.noreply.github.com>
2021-01-25 10:31:06 +0100

    Update main.tf

--- Terraform_jenkis_server/main.tf
+++ Terraform_jenkis_server/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-resource "aws_security_group" "default"{
+resource "security_group_jenkins_Nathan" "default"{
     vpc_id           = var.vpc_id
  ingress {
      description     = "Allow ssh"
